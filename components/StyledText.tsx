a6be098ae2690cb1c22840d38c1838dc222859c8
Daniel Lima <danieldeveloper1@gmail.com>
2024-08-27 22:40:06 -0300

    feat: cleaner desnecessary archives 😊

--- components/StyledText.tsx
+++ components/StyledText.tsx
@@ -1,5 +1,12 @@
-import { Text, TextProps } from './Themed';
+import { Text } from "react-native";
+
+type ThemeProps = {
+  lightColor?: string;
+  darkColor?: string;
+};
+
+export type TextProps = ThemeProps & Text["props"];
 
 export function MonoText(props: TextProps) {
-  return <Text {...props} style={[props.style, { fontFamily: 'SpaceMono' }]} />;
+  return <Text {...props} style={[props.style, { fontFamily: "SpaceMono" }]} />;
 }
