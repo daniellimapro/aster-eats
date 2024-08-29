4c4d5b5ad17d00f66b88e01ea569a66d1230381d
Daniel Lima <danieldeveloper1@gmail.com>
2024-08-28 23:12:07 -0300

    feat: cleaner and rename

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -1,20 +1,8 @@
 import { useEffect, useState } from "react";
 import { Product } from "./Product";
 import { FlatList, Text } from "native-base";
-import { useDishes } from "@/queries/useFoods";
+import { useDishes } from "@/queries/useDishes";
 import { ProductSkeleton } from "./ProductSkeleton";
-
-const DATA = [
-  { id: "1", title: "Item 1" },
-  { id: "2", title: "Item 2" },
-  { id: "3", title: "Item 3" },
-  { id: "4", title: "Item 4" },
-  { id: "5", title: "Item 5" },
-  { id: "6", title: "Item 6" },
-];
-
-// FOOD API EXAMPLE
-// https://dummyjson.com/recipes
 
 export const ProductList = () => {
   const { data, isLoading, isPending } = useDishes();
@@ -24,7 +12,7 @@ export const ProductList = () => {
       <FlatList
         data={[1, 2, 3]}
         renderItem={({ item }) => <ProductSkeleton />}
-        keyExtractor={(item) => item.id}
+        keyExtractor={(item) => item.toString()}
       />
     );
   }
