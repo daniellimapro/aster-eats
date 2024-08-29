1674ffbd8dea5634855044f1b815de34b03f81e6
Daniel Lima <danieldeveloper1@gmail.com>
2024-08-29 13:28:21 -0300

    feat: ToastAlert component 👀

---
+++ components/ToastAlert.tsx
@@ -0,0 +1,84 @@
+import {
+  Alert,
+  CloseIcon,
+  HStack,
+  IconButton,
+  useToast,
+  VStack,
+} from "native-base";
+import { Text } from "native-base";
+
+export const ToastAlert = ({
+  id,
+  status,
+  variant,
+  title,
+  description,
+  isClosable,
+  ...rest
+}) => {
+  const toast = useToast();
+
+  return (
+    <Alert
+      maxWidth="95%"
+      alignSelf="center"
+      flexDirection="row"
+      status={status ? status : "success"}
+      variant={variant}
+      {...rest}
+      bg="#875304"
+      marginTop={90}
+    >
+      <VStack space={1} flexShrink={1} w="100%">
+        <HStack
+          flexShrink={1}
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <HStack space={2} flexShrink={1} alignItems="center">
+            <Alert.Icon />
+            <Text
+              fontSize="md"
+              fontWeight="medium"
+              flexShrink={1}
+              color={
+                variant === "solid"
+                  ? "lightText"
+                  : variant !== "outline"
+                  ? "darkText"
+                  : null
+              }
+            >
+              {title}
+            </Text>
+          </HStack>
+          {isClosable ? (
+            <IconButton
+              variant="unstyled"
+              icon={<CloseIcon size="3" />}
+              _icon={{
+                color: variant === "solid" ? "lightText" : "darkText",
+              }}
+              onPress={() => toast.close(id)}
+            />
+          ) : null}
+        </HStack>
+        {description && (
+          <Text
+            px="6"
+            color={
+              variant === "solid"
+                ? "lightText"
+                : variant !== "outline"
+                ? "darkText"
+                : null
+            }
+          >
+            {description}
+          </Text>
+        )}
+      </VStack>
+    </Alert>
+  );
+};
--- context/CartContext.tsx
+++ context/CartContext.tsx
@@ -1,15 +1,7 @@
 import React, { createContext, useContext, useState, ReactNode } from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import {
-  Alert,
-  CloseIcon,
-  HStack,
-  IconButton,
-  Toast,
-  useToast,
-  VStack,
-} from "native-base";
+import { useToast } from "native-base";
 import { Text } from "native-base";
+import { ToastAlert } from "@/components/ToastAlert";
 
 // Definindo a interface para o item do carrinho
 interface CartItem {
@@ -26,81 +18,6 @@ interface CartContextType {
   addAmountToProduct: (itemToRemove: CartItem) => void;
   decreaseAmountOfProduct: (itemToRemove: CartItem) => void;
 }
-
-const ToastAlert = ({
-  id,
-  status,
-  variant,
-  title,
-  description,
-  isClosable,
-  ...rest
-}) => {
-  const toast = useToast();
-
-  return (
-    <Alert
-      maxWidth="95%"
-      alignSelf="center"
-      flexDirection="row"
-      status={status ? status : "success"}
-      variant={variant}
-      {...rest}
-      bg="#875304"
-      marginTop={90}
-    >
-      <VStack space={1} flexShrink={1} w="100%">
-        <HStack
-          flexShrink={1}
-          alignItems="center"
-          justifyContent="space-between"
-        >
-          <HStack space={2} flexShrink={1} alignItems="center">
-            <Alert.Icon />
-            <Text
-              fontSize="md"
-              fontWeight="medium"
-              flexShrink={1}
-              color={
-                variant === "solid"
-                  ? "lightText"
-                  : variant !== "outline"
-                  ? "darkText"
-                  : null
-              }
-            >
-              {title}
-            </Text>
-          </HStack>
-          {isClosable ? (
-            <IconButton
-              variant="unstyled"
-              icon={<CloseIcon size="3" />}
-              _icon={{
-                color: variant === "solid" ? "lightText" : "darkText",
-              }}
-              onPress={() => toast.close(id)}
-            />
-          ) : null}
-        </HStack>
-        {description && (
-          <Text
-            px="6"
-            color={
-              variant === "solid"
-                ? "lightText"
-                : variant !== "outline"
-                ? "darkText"
-                : null
-            }
-          >
-            {description}
-          </Text>
-        )}
-      </VStack>
-    </Alert>
-  );
-};
 
 // Criando o contexto
 const CartContext = createContext<CartContextType | undefined>(undefined);
