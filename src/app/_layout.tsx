bf19c09e8576f0c47ef2a6531a40458d000f3303
Daniel Lima <danieldeveloper1@gmail.com>
2024-08-30 11:18:28 -0300

    feat: change folder architeture

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -30,7 +30,7 @@ const queryClient = new QueryClient({
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    SpaceMono: require("../../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
 
