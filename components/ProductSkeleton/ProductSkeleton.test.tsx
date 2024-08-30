5bbec0f869d4ba55af6fd537e1cd08091ab67d4f
Daniel Lima <danieldeveloper1@gmail.com>
2024-08-30 09:36:47 -0300

    feat: cleaner import

--- components/ProductSkeleton/ProductSkeleton.test.tsx
+++ components/ProductSkeleton/ProductSkeleton.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react-native";
+import { render } from "@testing-library/react-native";
 import { ProductSkeleton } from "./ProductSkeleton";
 import { NativeBaseProvider } from "native-base";
 
